100baf4f86dfd12e41c58f570957394cb18d9da1
Thakran <prashantthakran0664@gmail.com>
2023-01-22 14:09:39 +0530

    lcs

--- longest common substring.cpp
+++ longest common substring.cpp
@@ -12,7 +12,7 @@ class Solution{
         vector<vector<int>>arr(n+1,vector<int>(m+1,0));
         int ans=0;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<-m;j++){
+            for(int j=1;j<=m;j++){
                 if(S1[i-1]==S2[j-1]){
                     arr[i][j]=1+arr[i-1][j-1];
                     ans=max(ans,arr[i][j]);
