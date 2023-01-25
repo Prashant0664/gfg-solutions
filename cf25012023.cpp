75288677a0bae715d57ce4ebcde181dd049517b2
Thakran <prashantthakran0664@gmail.com>
2023-01-25 21:09:18 +0530

    24-Jan-2023

--- cf25012023.cpp
+++ cf25012023.cpp
@@ -1,5 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int od=0;
+        int ev=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                ev++;
+            }
+            else{
+                od++;
+            }
+        }
+        
+                // cout<<"_"<<od<<"-";
+        if(ev==n||(od==n&&3%2==0)||(3==n&&od%2==0)){
+            cout<<"NO";
+        }
+        else{
+            cout<<"YES"<<endl;
+                
+            vector<int>ans;
+            if(ev>=2){
+                int h=0;
+                for(int i=0;i<n;i++){
+                    if(h==2){
+                        continue;
+                    }
+                    else{
+                        if(arr[i]%2==0){
+                            h++;
+                            ans.push_back(i+1);
+                        }
+                    }
+                }
+                int c=0;
+                for(int i=0;i<n;i++){
+                    if(c>1){
+                        continue;
+                    }
+                    if(arr[i]%2!=0){
+                        c++;
+                        ans.push_back(i+1);
+                    }
+                }
+                for(int i=0;i<3;i++){
+                    cout<<ans[i]<<" ";
+                }
+            }
+            else{
+                int c=0;
+                for(int i=0;i<n;i++){
+                    if(c>3){
+                        break;
+                    }
+                    else{
+                        if(arr[i]%2!=0){
+                            c++;
+                            ans.push_back(i+1);
+                        }
+                    }
+                }
+                
+                for(int i=0;i<3;i++){
+                    cout<<ans[i]<<" ";
+                }
+            }
+            
+        }
+        cout<<endl;
+    }
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,82 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int  main(){
+int main(){
     int t;
     cin>>t;
     while(t--){
         int n;
         cin>>n;
-        int arr[n]={};
+        int arr[n];
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
-        int ans=0;
-        for(int i=0;i<n-1;i++){
-            if(arr[i]%2==0&&arr[i+1]%2==0){
-                ans++;
-            }
-            else if(arr[i]%2==1&&arr[i+1]%2==1){
-                ans++;
+        int od=0;
+        int ev=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                ev++;
             }
             else{
-                ;
+                od++;
             }
         }
-        cout<<ans<<endl;
+        
+                // cout<<"_"<<od<<"-";
+        if(ev==n||(od==n&&3%2==0)||(3==n&&od%2==0)){
+            cout<<"NO";
+        }
+        else{
+            cout<<"YES"<<endl;
+                
+            vector<int>ans;
+            if(ev>=2){
+                int h=0;
+                for(int i=0;i<n;i++){
+                    if(h==2){
+                        continue;
+                    }
+                    else{
+                        if(arr[i]%2==0){
+                            h++;
+                            ans.push_back(i+1);
+                        }
+                    }
+                }
+                int c=0;
+                for(int i=0;i<n;i++){
+                    if(c>1){
+                        continue;
+                    }
+                    if(arr[i]%2!=0){
+                        c++;
+                        ans.push_back(i+1);
+                    }
+                }
+                for(int i=0;i<3;i++){
+                    cout<<ans[i]<<" ";
+                }
+            }
+            else{
+                int c=0;
+                for(int i=0;i<n;i++){
+                    if(c>3){
+                        break;
+                    }
+                    else{
+                        if(arr[i]%2!=0){
+                            c++;
+                            ans.push_back(i+1);
+                        }
+                    }
+                }
+                
+                for(int i=0;i<3;i++){
+                    cout<<ans[i]<<" ";
+                }
+            }
+            
+        }
+        cout<<endl;
     }
     return 0;
 }
