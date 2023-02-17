7e722382328bb8bccbef7ded8b0512b5e6507ccd
Thakran <prashantthakran0664@gmail.com>
2023-02-17 21:35:21 +0530

    43t3435

---
+++ repeating_element.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace  std;
+int main(){
+    for(int i=1;i<32;i++){
+        cout<<(i>>1)<<" ";
+    }
+    // cout<<(1&2)
+    // cout<<(1^2^3^3^4^5)<<endl;
+    // cout<<(1^2^3^4^5^6);
+    return 0;
+}
